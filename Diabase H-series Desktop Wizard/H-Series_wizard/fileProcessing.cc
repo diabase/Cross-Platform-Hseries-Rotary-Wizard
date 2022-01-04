1cd3eb0fd5ca8e2fa67e116bb31823ddec635bcf
ncolleran2 <ncolleran2@huskers.unl.edu>
2022-01-04 14:20:43 -0700

    Improved all c++ code
    Made all the c++ code prettier and more readable. Additionally, removed an unused c++ file

--- Diabase H-series Desktop Wizard/H-Series_wizard/fileProcessing.cc
+++ Diabase H-series Desktop Wizard/H-Series_wizard/fileProcessing.cc
@@ -22,55 +22,58 @@ Copyright (c) 2021. Nick Colleran, Diabase Engineering, LLC
 
 #define _CRT_SECURE_NO_WARNINGS
 #define _USE_MATH_DEFINES
- 
+
 #include <cmath>
+
 #include <iostream>
+
 #include <vector>
+
 #include <array>
+
 #include <string>
+
 #include <stdint.h>
+
 #include "stl_reader.h"
+
 #include <fstream>
 
 //Prototype
-void write_stl(std::string filename, std::vector<unsigned int> tris, std::vector<float> coords, double stretch);
-void rotaryTransform(float* p, double stretch);
-void editGcode(std::string inFileName,std::string outFileName, double radius);
-
+void write_stl(std::string filename, std::vector < unsigned int > tris, std::vector < float > coords, double stretch);
+void rotaryTransform(float * p, double stretch);
+void editGcode(std::string inFileName, std::string outFileName, double radius);
 
 /* This is the stl processing method. It gets called with:
 const addon = require('../build/Release/fileProcessing.node');
 addon.processSTL(PathOfOriginalStl, PathOfOutputLocation, StretchFactor)
 */
-void Add(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-v8::Local<v8::Context> context = info.GetIsolate()->GetCurrentContext();
+void Add(const Nan::FunctionCallbackInfo < v8::Value > & info) {
+  v8::Local < v8::Context > context = info.GetIsolate() -> GetCurrentContext();
 
-if (info.Length() != 3) {
-	Nan::ThrowTypeError("Required parameters: InFileName, OutFileName, Stretch");
-	return;
-}
+  if (info.Length() != 3) {
+    Nan::ThrowTypeError("Required parameters: InFileName, OutFileName, Stretch");
+    return;
+  }
 
-v8::Isolate* isolate = info.GetIsolate();
-v8::String::Utf8Value utf8Infilename(isolate, info[0]);
-std::string infilename(*utf8Infilename);
-v8::String::Utf8Value utf8Outfilename(isolate, info[1]);
-std::string outfilename(*utf8Outfilename);
-int stretch = info[2]->NumberValue(context).FromJust();
-std::vector<float> coords, normals;
-std::vector<unsigned int> tris, solids;
+  v8::Isolate * isolate = info.GetIsolate();
+  v8::String::Utf8Value utf8Infilename(isolate, info[0]);
+  std::string infilename( * utf8Infilename);
+  v8::String::Utf8Value utf8Outfilename(isolate, info[1]);
+  std::string outfilename( * utf8Outfilename);
+  int stretch = info[2] -> NumberValue(context).FromJust();
+  std::vector < float > coords, normals;
+  std::vector < unsigned int > tris, solids;
 
-try
-	{
-		std::string extraExtension;
-		stl_reader::ReadStlFile((infilename).c_str(), coords, normals, tris, solids);
-		write_stl(outfilename, tris, coords, stretch);
-		info.GetReturnValue().Set(true);
-	}
-	catch (std::exception& e)
-	{
-		std::cout << e.what() << std::endl;
-		info.GetReturnValue().Set(false);
-	}
+  try {
+    std::string extraExtension;
+    stl_reader::ReadStlFile((infilename).c_str(), coords, normals, tris, solids);
+    write_stl(outfilename, tris, coords, stretch);
+    info.GetReturnValue().Set(true);
+  } catch (std::exception & e) {
+    std::cout << e.what() << std::endl;
+    info.GetReturnValue().Set(false);
+  }
 }
 
 
@@ -78,66 +81,69 @@ try
 This is the method that writes to the STL File.
 It is called within the Add method.
 */
-void write_stl(std::string filename, std::vector<unsigned int> tris, std::vector<float> coords, double stretch)
-{	
-	//binary file
-	std::string header_info = filename + " -output" + "\0";
-	char head[80];
-	std::strncpy(head, header_info.c_str(), sizeof(head));// - 1
-	char attribute[2] = "0";
-	unsigned long numTris = tris.size() / 3;
-	unsigned long numCor = coords.size() / 3;
+void write_stl(std::string filename, std::vector < unsigned int > tris, std::vector < float > coords, double stretch) {
+  //binary file
+  std::string header_info = filename + " -output" + "\0";
+  char head[80];
+  std::strncpy(head, header_info.c_str(), sizeof(head)); // - 1
+  char attribute[2] = "0";
+  unsigned long numTris = tris.size() / 3;
+  unsigned long numCor = coords.size() / 3;
 
-	std::ofstream myfile;
+  std::ofstream myfile;
 
-	myfile.open((filename).c_str(), std::ios::out | std::ios::binary);
-	myfile.write(head, sizeof(head));
-	myfile.write((char*)&numTris, sizeof(numTris));
+  myfile.open((filename).c_str(), std::ios::out | std::ios::binary);
+  myfile.write(head, sizeof(head));
+  myfile.write((char * ) & numTris, sizeof(numTris));
 
-	//go thru all the coords, transform
-	for (size_t icor = 0; icor < numCor; icor++) {
-		float* c = &coords[3 * icor];
-		rotaryTransform(c, stretch);
-	}
+  //go thru all the coords, transform
+  for (size_t icor = 0; icor < numCor; icor++) {
+    float * c = & coords[3 * icor];
+    rotaryTransform(c, stretch);
+  }
 
-	for (size_t itri = 0; itri < numTris; itri++){
-		float zed = 0;
-		for (size_t itr = 0; itr < 3; itr++){		//normals
-			myfile.write((char*)&zed, sizeof(float));
-		}
-		int cmap[3] = { 1, 0,  2 };
-		for (int icorner = 0; icorner < 3; icorner++){
-			float* c = &coords[3 * tris[3 * itri + cmap[icorner]]];
-			for (size_t itr = 0; itr < 3; itr++)
-				myfile.write((char*)&c[itr], sizeof(c[0]));
-		}
-		myfile.write(attribute, 2);
-	}
-	myfile.close();
+  for (size_t itri = 0; itri < numTris; itri++) {
+    float zed = 0;
+    for (size_t itr = 0; itr < 3; itr++) { //normals
+      myfile.write((char * ) & zed, sizeof(float));
+    }
+    int cmap[3] = {
+      1,
+      0,
+      2
+    };
+    for (int icorner = 0; icorner < 3; icorner++) {
+      float * c = & coords[3 * tris[3 * itri + cmap[icorner]]];
+      for (size_t itr = 0; itr < 3; itr++)
+        myfile.write((char * ) & c[itr], sizeof(c[0]));
+    }
+    myfile.write(attribute, 2);
+  }
+  myfile.close();
 }
 
 /*
 This makes the needed transformations for rotary printing. 
 */
-void rotaryTransform(float* p, double stretch) {
-	float rad = std::sqrt(p[1] * p[1] + p[2] * p[2]);
-	float alpha = std::atan2f(-p[1], p[2]) * stretch;
-	//p[0] unchanged
-	p[1] = rad * alpha;
-	p[2] = rad;
+void rotaryTransform(float * p, double stretch) {
+  float rad = std::sqrt(p[1] * p[1] + p[2] * p[2]);
+  float alpha = std::atan2f(-p[1], p[2]) * stretch;
+  //p[0] unchanged
+  p[1] = rad * alpha;
+  p[2] = rad;
 }
 
 
 /*
 This exports the add method to be used as "processSTL".
 */
-void Init(v8::Local<v8::Object> exports) {
-v8::Local<v8::Context> context = exports->CreationContext();
-exports->Set(context,
-			Nan::New("processSTL").ToLocalChecked(),
-			Nan::New<v8::FunctionTemplate>(Add)
-				->GetFunction(context)
-				.ToLocalChecked());
+void Init(v8::Local < v8::Object > exports) {
+  v8::Local < v8::Context > context = exports -> CreationContext();
+  exports -> Set(context,
+    Nan::New("processSTL").ToLocalChecked(),
+    Nan::New < v8::FunctionTemplate > (Add) -
+    > GetFunction(context)
+    .ToLocalChecked());
 }
 
 NODE_MODULE(stlProcessing, Init)
--- Diabase H-series Desktop Wizard/H-Series_wizard/gcodeProcessing.cc
+++ Diabase H-series Desktop Wizard/H-Series_wizard/gcodeProcessing.cc
@@ -19,19 +19,26 @@ Copyright (c) 2021. Nick Colleran, Diabase Engineering, LLC
 
 #define _CRT_SECURE_NO_WARNINGS
 #define _USE_MATH_DEFINES
- 
+
 #include <cmath>
+
 #include <iostream>
+
 #include <vector>
+
 #include <array>
+
 #include <string>
+
 #include <sstream>
+
 #include <stdint.h>
+
 #include <fstream>
 
 # define M_PI 3.14159265358979323846
 
-void write_gcode(std::string outFileName,std::string inFileName, double radius);
+void write_gcode(std::string outFileName, std::string inFileName, double radius);
 std::string scaleY(std::string line, double radius, double currentLayerHeight, double firstLayerHeight);
 
 
@@ -39,174 +46,167 @@ std::string scaleY(std::string line, double radius, double currentLayerHeight, d
 const addon = require('../build/Release/gcodeProcessing.node');
 addon.gcodeProcessing(PathOfOriginalGcode, PathOfOutputLocation, Radius)
 */
-void Add(const Nan::FunctionCallbackInfo<v8::Value>& info) {
-  v8::Local<v8::Context> context = info.GetIsolate()->GetCurrentContext();
+void Add(const Nan::FunctionCallbackInfo < v8::Value > & info) {
+  v8::Local < v8::Context > context = info.GetIsolate() -> GetCurrentContext();
 
-if (info.Length() != 3) {
-	Nan::ThrowTypeError("Required parameters: InFileName, OutFileName, radius");
-	return;
-}
+  if (info.Length() != 3) {
+    Nan::ThrowTypeError("Required parameters: InFileName, OutFileName, radius");
+    return;
+  }
 
-v8::Isolate* isolate = info.GetIsolate();
-v8::String::Utf8Value utf8Infilename(isolate, info[0]);
-std::string infilename(*utf8Infilename);
-v8::String::Utf8Value utf8Outfilename(isolate, info[1]);
-std::string outfilename(*utf8Outfilename);
-double radius = info[2]->NumberValue(context).FromJust();
+  v8::Isolate * isolate = info.GetIsolate();
+  v8::String::Utf8Value utf8Infilename(isolate, info[0]);
+  std::string infilename( * utf8Infilename);
+  v8::String::Utf8Value utf8Outfilename(isolate, info[1]);
+  std::string outfilename( * utf8Outfilename);
+  double radius = info[2] -> NumberValue(context).FromJust();
 
-try
-	{
-		write_gcode(outfilename, infilename, radius);
-		info.GetReturnValue().Set(true);
-	}
-	catch (std::exception& e)
-	{
-		std::cout << e.what() << std::endl;
-		info.GetReturnValue().Set(false);
-	}
+  try {
+    write_gcode(outfilename, infilename, radius);
+    info.GetReturnValue().Set(true);
+  } catch (std::exception & e) {
+    std::cout << e.what() << std::endl;
+    info.GetReturnValue().Set(false);
+  }
 }
 
 /*
 This is the method that writes to the Gcode File.
 It is called within the Add method.
 */
-void write_gcode(std::string outFileName,std::string inFileName, double radius)
-{	
-	std::ifstream inFile;
-    inFile.open((inFileName).c_str(), std::ios::in);
+void write_gcode(std::string outFileName, std::string inFileName, double radius) {
+  std::ifstream inFile;
+  inFile.open((inFileName).c_str(), std::ios:: in );
 
-    std::ofstream outFile;
-	outFile.open((outFileName).c_str(), std::ios::out | std::ofstream::trunc);
-	outFile<< ";Processed with Diabase Rotary Printing Wizard.\n";
+  std::ofstream outFile;
+  outFile.open((outFileName).c_str(), std::ios::out | std::ofstream::trunc);
+  outFile << ";Processed with Diabase Rotary Printing Wizard.\n";
 
-    std::string currentLine;
+  std::string currentLine;
 
-    double mainLayerHeight = 0, firstLayerHeight = 0, currentLayerHeight = 0;
+  double mainLayerHeight = 0, firstLayerHeight = 0, currentLayerHeight = 0;
+
+  while (std::getline(inFile, currentLine)) {
+    if (currentLine.find(";Layer height: ") != std::string::npos) { // Cura Check for main layer height
+      std::size_t posOfLayerHeight = currentLine.find(";Layer height: ");
+      std::string layerHeightString = currentLine.substr(posOfLayerHeight + 15);
+      mainLayerHeight = std::stod(layerHeightString);
+      break;
+    } else if (currentLine.find(";   layerHeight,") != std::string::npos) { // Simplify 3d Check for main layer height
+      std::size_t posOfLayerHeight = currentLine.find(";   layerHeight,");
+      std::string layerHeightString = currentLine.substr(posOfLayerHeight + 16);
+      mainLayerHeight = std::stod(layerHeightString);
+      break;
+    }
+  }
+
+  while (std::getline(inFile, currentLine)) {
+    if (currentLine.find(";MINZ:") != std::string::npos) { // Cura Check for first layer height
+      std::size_t posOfFirstLayerHeight = currentLine.find(";MINZ:");
+      std::string firstLayerHeightString = currentLine.substr(posOfFirstLayerHeight + 6);
+      firstLayerHeight = std::stod(firstLayerHeightString);
+      currentLayerHeight = firstLayerHeight - mainLayerHeight;
+      break;
+    } else if (currentLine.find("; layer") != std::string::npos) { // Simplify 3d Check for first layer height
+      std::size_t posOfFirstLayerHeight = currentLine.find("; layer 1, Z = ");
+      std::string firstLayerHeightString = currentLine.substr(posOfFirstLayerHeight + 15);
+      firstLayerHeight = std::stod(firstLayerHeightString);
+      currentLayerHeight = firstLayerHeight;
+      break;
+    }
+  }
+
+  // Print error info to gcode file if needed info is not found
+  if (firstLayerHeight == 0) {
+    outFile << ";Could not find/calculate first layer height from Gcode. Process Failed.\n";
+  } else if (mainLayerHeight == 0) {
+    outFile << ";Could not find/calculate main layer height from Gcode. Process Failed.\n";
+  } else {
+    //Print found information to gcode file if all is well
+    outFile << ";Found Layer Height: ";
+    outFile << mainLayerHeight;
+    outFile << "\n";
+    outFile << ";Found First Layer Height: ";
+    outFile << firstLayerHeight;
+    outFile << "\n\n";
 
     while (std::getline(inFile, currentLine)) {
-        if (currentLine.find(";Layer height: ")!= std::string::npos){ // Cura Check for main layer height
-            std::size_t posOfLayerHeight = currentLine.find(";Layer height: ");
-            std::string layerHeightString = currentLine.substr(posOfLayerHeight + 15);
-            mainLayerHeight = std::stod(layerHeightString);
-            break;
-        }
-        else if (currentLine.find(";   layerHeight,")!= std::string::npos){ // Simplify 3d Check for main layer height
-            std::size_t posOfLayerHeight = currentLine.find(";   layerHeight,");
-            std::string layerHeightString = currentLine.substr(posOfLayerHeight + 16);
-            mainLayerHeight = std::stod(layerHeightString);
-            break;
-        }
+      if (currentLine.find(";LAYER:") != std::string::npos) {
+        currentLayerHeight += mainLayerHeight;
+      } else if (currentLine.find("; layer ") != std::string::npos) { //For each layer change after the initial layer, add the uniform layer height to the current z height
+        currentLayerHeight += mainLayerHeight;
+      }
+      outFile << scaleY(currentLine, radius, currentLayerHeight, firstLayerHeight) << "\n"; // MACOS: outFile<<scaleY(currentLine, radius, currentLayerHeight, firstLayerHeight);
     }
+  }
+  outFile << ";File Complete\n";
 
-    while (std::getline(inFile, currentLine)) {
-        if (currentLine.find(";MINZ:")!= std::string::npos){ // Cura Check for first layer height
-            std::size_t posOfFirstLayerHeight = currentLine.find(";MINZ:");
-            std::string firstLayerHeightString = currentLine.substr(posOfFirstLayerHeight + 6);
-            firstLayerHeight = std::stod(firstLayerHeightString);
-            currentLayerHeight = firstLayerHeight-mainLayerHeight;
-            break;
-        }
-        else if (currentLine.find("; layer")!= std::string::npos){ // Simplify 3d Check for first layer height
-            std::size_t posOfFirstLayerHeight = currentLine.find("; layer 1, Z = ");
-            std::string firstLayerHeightString = currentLine.substr(posOfFirstLayerHeight + 15);
-            firstLayerHeight = std::stod(firstLayerHeightString);
-            currentLayerHeight = firstLayerHeight;
-            break;
-        }
-    }
-
-    // Print error info to gcode file if needed info is not found
-    if(firstLayerHeight == 0){
-        outFile<< ";Could not find/calculate first layer height from Gcode. Process Failed.\n";
-    }else if(mainLayerHeight == 0){
-        outFile<< ";Could not find/calculate main layer height from Gcode. Process Failed.\n";
-    }else{
-        //Print found information to gcode file if all is well
-        outFile<< ";Found Layer Height: ";
-        outFile<< mainLayerHeight;
-        outFile<< "\n";
-        outFile<< ";Found First Layer Height: ";
-        outFile<< firstLayerHeight;
-        outFile<< "\n\n";
-
-        while (std::getline(inFile, currentLine)) {
-            if (currentLine.find(";LAYER:") != std::string::npos){
-                currentLayerHeight += mainLayerHeight;
-            }
-            else if (currentLine.find("; layer ") != std::string::npos){ //For each layer change after the initial layer, add the uniform layer height to the current z height
-                currentLayerHeight += mainLayerHeight;
-            }
-            outFile<<scaleY(currentLine, radius, currentLayerHeight, firstLayerHeight)<<"\n"; // MACOS: outFile<<scaleY(currentLine, radius, currentLayerHeight, firstLayerHeight);
-        }
-    }
-    outFile<< ";File Complete\n";
-
-    outFile.close();
-    inFile.close();
+  outFile.close();
+  inFile.close();
 }
 
 /*
 This is the method that can find the needed changes within a single gcode line.
 It returns the changed line to be added to the output file.
 */
-std::string scaleY(std::string line, double radius, double currentLayerHeight, double firstLayerHeight){
-    
-    double zFactor = radius+currentLayerHeight-firstLayerHeight;
-    double scaleFactor = 180/(zFactor*M_PI);
-    double testNumber;
-    line = line + " ";
-    std::string output = line;
-    std::string num = "";
-    bool Yflag = false;
-    bool Negflag = false;
-    if (line.find("G28") != std::string::npos){
-        return "G28 A";
-    }else if (line.find("G32") != std::string::npos){
-        return "G29 S1";
-    }
-        for (int i = 0; i < line.length(); i++) {
-            if(Yflag){
-                if(isdigit(line[i]) || line[i] == '.')
-                    num += line[i];
-                else{
-                    if(line[i] == '-'){
-                        Negflag = true;
-                    }else{
-                        Yflag = false;
-                        if(num.length() > 0){
-                            float number = std::stof(num);
-                            testNumber = number;
-                            number = number * scaleFactor;
-                            std::ostringstream ss;
-                            ss << number;
-                            std::string s(ss.str());
-                            if(Negflag){
-                                output = output.substr(0,i-num.length()-2) + "A-" + s + output.substr(i,output.length());
-                                Negflag = false; 
-                            }else{
-                                output = output.substr(0,i-num.length()-1) + "A" + s + output.substr(i,output.length());
-                            }
-                        }
-                    }
-                }
-            }
-            if(line[i] == 'Y' && line[i+1] != ' ')
-                Yflag = true;
-        }
+std::string scaleY(std::string line, double radius, double currentLayerHeight, double firstLayerHeight) {
 
-return output;
+  double zFactor = radius + currentLayerHeight - firstLayerHeight;
+  double scaleFactor = 180 / (zFactor * M_PI);
+  double testNumber;
+  line = line + " ";
+  std::string output = line;
+  std::string num = "";
+  bool Yflag = false;
+  bool Negflag = false;
+  if (line.find("G28") != std::string::npos) {
+    return "G28 A";
+  } else if (line.find("G32") != std::string::npos) {
+    return "G29 S1";
+  }
+  for (int i = 0; i < line.length(); i++) {
+    if (Yflag) {
+      if (isdigit(line[i]) || line[i] == '.')
+        num += line[i];
+      else {
+        if (line[i] == '-') {
+          Negflag = true;
+        } else {
+          Yflag = false;
+          if (num.length() > 0) {
+            float number = std::stof(num);
+            testNumber = number;
+            number = number * scaleFactor;
+            std::ostringstream ss;
+            ss << number;
+            std::string s(ss.str());
+            if (Negflag) {
+              output = output.substr(0, i - num.length() - 2) + "A-" + s + output.substr(i, output.length());
+              Negflag = false;
+            } else {
+              output = output.substr(0, i - num.length() - 1) + "A" + s + output.substr(i, output.length());
+            }
+          }
+        }
+      }
+    }
+    if (line[i] == 'Y' && line[i + 1] != ' ')
+      Yflag = true;
+  }
+
+  return output;
 }
 
 /*
 This exports the add method to be used as "gcodeProcessing".
 */
-void Init(v8::Local<v8::Object> exports) {
-  v8::Local<v8::Context> context = exports->CreationContext();
-  exports->Set(context,
-               Nan::New("gcodeProcessing").ToLocalChecked(),
-               Nan::New<v8::FunctionTemplate>(Add)
-                   ->GetFunction(context)
-                   .ToLocalChecked());
+void Init(v8::Local < v8::Object > exports) {
+  v8::Local < v8::Context > context = exports -> CreationContext();
+  exports -> Set(context,
+    Nan::New("gcodeProcessing").ToLocalChecked(),
+    Nan::New < v8::FunctionTemplate > (Add) -
+    > GetFunction(context)
+    .ToLocalChecked());
 }
 
 NODE_MODULE(addon, Init)
